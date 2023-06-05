b6ed3891bb7a371cfa740085fba4938364468a6a
Aradhana1505 <80398783+Aradhana1505@users.noreply.github.com>
2023-06-05 18:31:26 +0530

    Delete Microservices Project Work directory

--- Microservices Project Work/Process-pension-ms1/src/main/java/com/process/repo/ProcessPensionRepo.java
+++
@@ -1,9 +0,0 @@
-package com.process.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.process.entity.ProcessPensionInput;
-
-public interface ProcessPensionRepo extends JpaRepository<ProcessPensionInput, String>{
-
-}
--- Microservices Project Work/Process-pension-ms1/src/main/java/com/process/service/IProcessPension.java
+++
@@ -1,8 +0,0 @@
-package com.process.service;
-
-//import com.process.entity.ProcessPensionInput;
-
-public interface IProcessPension {
-
-	public double calculatePension(String aadharNumber);
-}
